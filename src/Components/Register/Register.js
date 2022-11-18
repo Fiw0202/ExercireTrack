b1368ea80ddc32a2e4020904555f082f6e11a0af
kanyarat-j <kanyarat.jirakul@gmail.com>
2022-11-19 01:47:18 +0700

    edit register

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,25 +1,26 @@
+import './Register.css'
 
 const Register = () => {
     return (
         <div>
-            <form>
+            <form className='register-container'>
                 <span>Register Account Form</span>
-                <div className="register-form">
+                <div className="register-name">
                     <input type="text" placeholder="Name" />
                     <input type="text" placeholder="Surname" />
                 </div>
-                <div className="register-form">
+                <div className="register-form center">
                     <input type="text" placeholder="Username" /><br />
                     <input type="text" placeholder="Email" /><br />
-                    <input type="text" placeholder="Password" /><br />
-                    <input type="text" placeholder="Confirm-Password" />
+                    <input type="password" placeholder="Password" /><br />
+                    <input type="password" placeholder="Confirm-Password" />
                 </div>
-                <div className="register-form">
+                <div className="register-checkbox">
                     <label className="checkbox"><br />
                     <input type="checkbox" />I agree with the terms and conditions and the Privacy Policy</label>
                 </div>
-                <div className="register-button">
-                    <button>Create Account</button>
+                <div className="register-button center">
+                    <button type="submit">Create Account</button>
                 </div>
             </form>
         </div>
