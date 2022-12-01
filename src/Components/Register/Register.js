54c724ad6d94d70866e87f83d6864d3d0aaa69bb
kanyarat-j <kanyarat.jirakul@gmail.com>
2022-12-01 14:42:48 +0700

    edit login and register

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,27 +1,40 @@
-import './Register.css'
+import "./Register.css";
+import React, { useState } from 'react';
+
 
 const Register = () => {
-    return (
-        <div>
-            <form className='register-container'>
-                <span>Register Account Form</span>
-                <div className="register-name">
-                    <input type="text" placeholder="Name" />
-                    <input type="text" placeholder="Surname" />
-                </div>
-                <div className="register-form center">
-                    <input type="text" placeholder="Username" /><br />
-                    <input type="text" placeholder="Email" /><br />
-                    <input type="password" placeholder="Password" /><br />
-                    <input type="password" placeholder="Confirm-Password" />
-                </div>
-             
-                <div className="register-button center">
-                    <button type="submit">Create Account</button>
-                </div>
-            </form>
-        </div>
-    )
-}
+    const [formField, setFormField] = useState({
+        name: '',
 
-export default Register
+    });
+    const formFieldHandler = (e)=>{
+        let fieldName = e.target.name
+        let value = e.target.value
+        console.log("e.target", e.target)
+        console.log("fieldName", fieldName)
+        console.log("value", value)
+        setFormField({...formField, [fieldName]:value})
+      }
+    
+  return (
+    <div className="register-container">
+      <form className="register-form">
+        <span className="register-head">Register</span>
+        <div className="register-form center">
+          <input type="text" name="name" placeholder="username" value={formField.username} onChange={formFieldHandler} />
+          formField {formField.name}
+          <input type="text" placeholder="Surname" />
+
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <input type="password" placeholder="Confirm-Password" />
+        </div>
+        <button type="submit" className="btn-submit">
+          Create Account
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
