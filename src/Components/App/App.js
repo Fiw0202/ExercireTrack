94b6466d36c06fa4e8e64708cae20db5fea570f6
Fiuse <Fiw0202@hotmail.com>
2022-12-09 13:46:05 +0700

    add MyProfile

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,11 @@
-import './App.css';
-import Piechart from '../Piechart/Piechart';
+import "./App.css";
+import Piechart from "../Piechart/Piechart";
+import MyProfile from "../MyProfile/MyProfile";
 function App() {
-  
   return (
     <div className="App">
-      <Piechart/>
+      {/*<MyProfile/>*/}
+      <Piechart />
     </div>
   );
 }
