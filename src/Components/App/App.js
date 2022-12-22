019a23634266aae466f7f6986e32d1ef7c3234db
Fiuse <Fiw0202@hotmail.com>
2022-12-22 16:13:58 +0700

    Please comment at piechart.js UseEffect for into /overview

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,69 +5,13 @@ import Register from '../Register/Register'
 import NavBar from '../NavBar/NavBar'
 import Piechart from '../Piechart/Piechart'
 import MyProfile from '../MyProfile/MyProfile'
-import NewExercire from "../NewExercire/NewExercire";
-import ActivitySum from "../ActivitySum/ActivitySum";
-import ActivityContext from "../../Data/ActivityContext";
 import { Routes , Route } from 'react-router-dom';
 
 
 function App() {
 
-  //MockUP Data
-  const data = [
-    {
-      id: 1,
-      activity: "Running",
-      date: "12/12/2565",
-      time: "12.00 น.",
-      sets: 6,
-      distance: " 10 ",
-      calories: "2000",
-    },
-    {
-      id: 2,
-      activity: "Walking",
-      date: "12/12/2565",
-      time: "18.00 น.",
-      sets: 3,
-      distance: " 1 ",
-      calories: "200",
-    },
-    {
-      id: 3,
-      activity: "Bicycling",
-      date: "12/12/2565",
-      time: "12.00 น.",
-      sets: 6,
-      distance: " 10 ",
-      calories: "2000",
-    },
-    {
-      id: 4,
-      activity: "Swimming",
-      date: "12/12/2565",
-      time: "12.00 น.",
-      sets: 6,
-      distance: " 10 ",
-      calories: "2000",
-    },
-    {
-      id: 5,
-      activity: "Hiking",
-      date: "12/12/2565",
-      time: "12.00 น.",
-      sets: 6,
-      distance: " 10 ",
-      calories: "2000",
-    },
-  ];
+  
 
-  const [item, setItem] = useState(data);
-  const onAddNewActivity = (newActivity) => {
-    setItem((prevCard) => {
-      return [newActivity, ...prevCard];
-    });
-  };
   
   const [token, setToken] = useState('');
 
@@ -80,12 +24,14 @@ function App() {
      
         <Route path="/" exact element={<Login setToken={setToken}/>}></Route>
         <Route path="/register" element={<Register />}></Route>
-        <Route path="/overview" element={<Piechart token={token}/>}></Route>
+        <Route path="/overview" element={<Piechart token={token}/>}>
+
+        </Route>
         <Route path="/profile" element={<MyProfile token={token}/>}></Route>
-        <ActivityContext.Provider value={"ActivityContext"}>
-        <Route path="/activitycard" element={<ActivitySum cards={item} token={token}/>}></Route>
-        <NewExercire onAddActivity={onAddNewActivity} />
-        </ActivityContext.Provider>
+      
+        {/*<Route path="/activitycard" element={<ActivitySum cards={item} token={token}/>}></Route>
+        <NewExercire onAddActivity={onAddNewActivity} />*/}
+ 
       
       </Routes>
     </div>
--- src/Components/Piechart/Piechart.js
+++ src/Components/Piechart/Piechart.js
@@ -1,8 +1,10 @@
-import {React, useEffect} from 'react';
+import {React, useEffect, useState} from 'react';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
 import { useNavigate } from 'react-router-dom'
 import './Piechart.css';
+import ActivitySum from '../ActivitySum/ActivitySum';
+import NewExercire from "../NewExercire/NewExercire";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
@@ -26,23 +28,83 @@ const data = {
   ],
 
 };
+//MockUP Data
+const items = [
+  {
+    id: 1,
+    activity: "Running",
+    date: "12/12/2565",
+    time: "12.00 น.",
+    sets: 6,
+    distance: " 10 ",
+    calories: "2000",
+  },
+  {
+    id: 2,
+    activity: "Walking",
+    date: "12/12/2565",
+    time: "18.00 น.",
+    sets: 3,
+    distance: " 1 ",
+    calories: "200",
+  },
+  {
+    id: 3,
+    activity: "Bicycling",
+    date: "12/12/2565",
+    time: "12.00 น.",
+    sets: 6,
+    distance: " 10 ",
+    calories: "2000",
+  },
+  {
+    id: 4,
+    activity: "Swimming",
+    date: "12/12/2565",
+    time: "12.00 น.",
+    sets: 6,
+    distance: " 10 ",
+    calories: "2000",
+  },
+  {
+    id: 5,
+    activity: "Hiking",
+    date: "12/12/2565",
+    time: "12.00 น.",
+    sets: 6,
+    distance: " 10 ",
+    calories: "2000",
+  },
+];
+
 
 function Piechart({ token }) {
+
+  
+  const [Item, setItem] = useState(items);
+  const onAddNewActivity = (newActivity) => {
+    setItem((prevCard) => {
+      return [newActivity, ...prevCard];
+    });
+  };
   const navigate = useNavigate();
 
   useEffect(() => {
     // Fetch the token from local storage
     // const token = localStorage.getItem('token');
     // If there is no token, redirect the user to the login page
-    if (!token) {
-      alert('Login first!')
-      navigate('/')
-    }
-
+    // if (!token) {
+    //   alert('Login first!')
+    //   navigate('/')
+    // }
+   
   }, [token]);
   
     return(
-        <div className="chart-data"><Pie data={data} /></div>
+        <div className="chart-data"><Pie data={data} />
+        <ActivitySum cards={Item} />
+        <NewExercire onAddActivity={onAddNewActivity} />
+        </div>
     ) ;
   }
 
